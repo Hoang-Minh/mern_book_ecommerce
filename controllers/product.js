af08d52a548ecfc46bd58249fab09b28174af35b
Minh Nguyen <minh.nguyen@ocit.oc.gov>
2026-05-15 11:08:23 -0700

    fix: formidable v3 API, deleteOne, fix typos, async/await in product controller

--- controllers/product.js
+++ controllers/product.js
@@ -4,54 +4,47 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.productById = (req, res, next, id) => {
-  Product.findById(id)
-    .populate("category")
-    .exec((error, product) => {
-      if (error || !product)
-        return res.status(400).json({ error: "Product not found" });
-
-      req.product = product;
-      next();
-    });
+exports.productById = async (req, res, next, id) => {
+  try {
+    const product = await Product.findById(id).populate("category");
+    if (!product) return res.status(400).json({ error: "Product not found" });
+    req.product = product;
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: "Product not found" });
+  }
 };
 
 exports.create = (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.keepExtensions = true; // keep the file extension
-  form.parse(req, (error, fields, files) => {
+  const form = new formidable.Formidable({ keepExtensions: true });
+  form.parse(req, async (error, fields, files) => {
     if (error)
       return res.status(400).json({ error: "Image could not be uploaded" });
 
-    // check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
+    const flatFields = Object.fromEntries(
+      Object.entries(fields).map(([key, val]) => [key, val?.[0]])
+    );
 
+    const { name, description, price, category, quantity, shipping } = flatFields;
     if (!name || !description || !price || !category || !quantity || !shipping)
       return res.status(400).json({ error: "All fields are required" });
 
-    const product = new Product(fields);
+    const product = new Product(flatFields);
 
-    // the "photo" name coming/depending from the client side
-    if (files.photo) {
-      //console.log("Files photo:", files.photo);
-      if (files.photo.size > 1000000)
-        return res
-          .satus(400)
-          .json({ error: "Image size must be less than 1 MB" });
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
+    const photo = files.photo?.[0];
+    if (photo) {
+      if (photo.size > 1000000)
+        return res.status(400).json({ error: "Image size must be less than 1 MB" });
+      product.photo.data = fs.readFileSync(photo.filepath);
+      product.photo.contentType = photo.mimetype;
     }
 
-    console.log("product controller - saved");
-
-    product.save((error, result) => {
-      if (error) {
-        console.log(error);
-        return res.status(400).json({ error: errorHandler(error) });
-      }
-
+    try {
+      const result = await product.save();
       res.json(result);
-    });
+    } catch (err) {
+      return res.status(400).json({ error: errorHandler(err) });
+    }
   });
 };
 
@@ -60,93 +53,88 @@ exports.read = (req, res) => {
   return res.json(req.product);
 };
 
-exports.remove = (req, res) => {
-  const product = req.product;
-  product.remove((error, deletedProuduct) => {
-    if (error) return res.status(400).json({ error: errorHandler(error) });
-
+exports.remove = async (req, res) => {
+  try {
+    await req.product.deleteOne();
     res.json({ message: "Product deleted successfully" });
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
 exports.update = (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.keepExtensions = true; // keep the file extension
-  form.parse(req, (error, fields, files) => {
+  const form = new formidable.Formidable({ keepExtensions: true });
+  form.parse(req, async (error, fields, files) => {
     if (error)
       return res.status(400).json({ error: "Image could not be uploaded" });
 
-    // check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
-
-    // if (!name || !description || !price || !category || !quantity || !shipping)
-    //   return res.status(400).json({ error: "All fields are required" });
+    const flatFields = Object.fromEntries(
+      Object.entries(fields).map(([key, val]) => [key, val?.[0]])
+    );
 
     let product = req.product;
-    product = _.extend(product, fields);
+    product = _.extend(product, flatFields);
 
-    // the "photo" name coming/depending from the client side
-    if (files.photo) {
-      //console.log("Files photo:", files.photo);
-      if (files.photo.size > 1000000)
-        return res
-          .satus(400)
-          .json({ error: "Image size must be less than 1 MB" });
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
+    const photo = files.photo?.[0];
+    if (photo) {
+      if (photo.size > 1000000)
+        return res.status(400).json({ error: "Image size must be less than 1 MB" });
+      product.photo.data = fs.readFileSync(photo.filepath);
+      product.photo.contentType = photo.mimetype;
     }
 
-    product.save((error, result) => {
-      if (error) return res.status(400).json({ error: errorHandler(error) });
-
+    try {
+      const result = await product.save();
       res.json(result);
-    });
+    } catch (err) {
+      return res.status(400).json({ error: errorHandler(err) });
+    }
   });
 };
 
-exports.list = (req, res) => {
-  const order = req.query.order ? req.query.order : "asc";
-  const sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+exports.list = async (req, res) => {
+  const order = req.query.order || "asc";
+  const sortBy = req.query.sortBy || "_id";
   const limit = req.query.limit ? parseInt(req.query.limit) : 6;
-
-  Product.find()
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .limit(limit)
-    .exec((error, products) => {
-      if (error) return res.status(400).json({ error: "Products not found" });
-
-      res.json(products); // res.send(products)
-    });
+  try {
+    const products = await Product.find()
+      .select("-photo")
+      .populate("category")
+      .sort([[sortBy, order]])
+      .limit(limit);
+    res.json(products);
+  } catch (error) {
+    return res.status(400).json({ error: "Products not found" });
+  }
 };
 
-// will return products based on the req product cateogry
-// other products that has the same category will be returned.
-exports.listRelated = (req, res) => {
+exports.listRelated = async (req, res) => {
   const limit = req.query.limit ? parseInt(req.query.limit) : 6;
-  Product.find({ _id: { $ne: req.product }, category: req.product.category })
-    .limit(limit)
-    .populate("category", "_id name")
-    .exec((error, products) => {
-      if (error) return res.status(400).json({ error: "Product not found" });
-
-      res.json(products);
-    });
+  try {
+    const products = await Product.find({
+      _id: { $ne: req.product._id },
+      category: req.product.category,
+    })
+      .limit(limit)
+      .populate("category", "_id name");
+    res.json(products);
+  } catch (error) {
+    return res.status(400).json({ error: "Product not found" });
+  }
 };
 
-exports.listCategories = (req, res) => {
-  Product.distinct("category", {}, (error, categories) => {
-    if (error) return res.status(400).json({ error: "Categories not found" });
-
+exports.listCategories = async (req, res) => {
+  try {
+    const categories = await Product.distinct("category");
     res.json(categories);
-  });
+  } catch (error) {
+    return res.status(400).json({ error: "Categories not found" });
+  }
 };
 
-// return list of products with filters
-exports.listBySearch = (req, res) => {
-  const order = req.body.order ? req.body.order : "desc";
-  const sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+exports.listBySearch = async (req, res) => {
+  const order = req.body.order || "desc";
+  const sortBy = req.body.sortBy || "_id";
   const limit = req.body.limit ? parseInt(req.body.limit) : 100;
   const skip = parseInt(req.body.skip);
   const findArgs = {};
@@ -164,23 +152,17 @@ exports.listBySearch = (req, res) => {
     }
   }
 
-  Product.find(findArgs)
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .skip(skip)
-    .limit(limit)
-    .exec((error, data) => {
-      if (error) {
-        return res.status(400).json({
-          error: "Products not found",
-        });
-      }
-      res.json({
-        size: data.length,
-        data,
-      });
-    });
+  try {
+    const data = await Product.find(findArgs)
+      .select("-photo")
+      .populate("category")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit);
+    res.json({ size: data.length, data });
+  } catch (error) {
+    return res.status(400).json({ error: "Products not found" });
+  }
 };
 
 exports.photo = (req, res, next) => {
@@ -188,46 +170,36 @@ exports.photo = (req, res, next) => {
     res.set("Content-Type", req.product.photo.contentType);
     return res.send(req.product.photo.data);
   }
-
   next();
 };
 
-exports.listSearch = (req, res) => {
-  // create query object to hold search value and category value
+exports.listSearch = async (req, res) => {
   const query = {};
-
-  // build query to search, option i - stands for incase sensitive
   if (req.query.search) {
     query.name = { $regex: req.query.search, $options: "i" };
-
-    // asign category value to query.category
-    // find the product based on query object with 2 properties: name and category. By default, category All will be selected in front end
     if (req.query.category && req.query.category !== "All") {
       query.category = req.query.category;
     }
-
-    Product.find(query, (error, products) => {
-      if (error) return res.status(400).json({ error: errorHandler(error) });
-
+    try {
+      const products = await Product.find(query).select("-photo");
       res.json(products);
-    }).select("-photo");
+    } catch (error) {
+      return res.status(400).json({ error: errorHandler(error) });
+    }
   }
 };
 
-exports.decreaseQuantity = (req, res, next) => {
-  const bulkOps = req.body.order.products.map((item) => {
-    return {
-      updateOne: {
-        filter: { _id: item._id },
-        update: { $inc: { quantity: -item.count, sold: +item.count } },
-      },
-    };
-  });
-
-  Product.bulkWrite(bulkOps, {}, (error, product) => {
-    if (error)
-      return res.status(400).json({ error: "Could not update product" });
-
+exports.decreaseQuantity = async (req, res, next) => {
+  const bulkOps = req.body.order.products.map((item) => ({
+    updateOne: {
+      filter: { _id: item._id },
+      update: { $inc: { quantity: -item.count, sold: +item.count } },
+    },
+  }));
+  try {
+    await Product.bulkWrite(bulkOps);
     next();
-  });
+  } catch (error) {
+    return res.status(400).json({ error: "Could not update product" });
+  }
 };
