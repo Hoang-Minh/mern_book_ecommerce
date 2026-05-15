96448404cdff91db8cc316c9cd15887f54ffe7e6
Minh Nguyen <minh.nguyen@ocit.oc.gov>
2026-05-15 11:08:43 -0700

    fix: async/await, Order.updateOne, and sort field in order controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,85 +1,76 @@
-const { Order, CartItem } = require("../models/order");
+const { Order } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 const User = require("../models/users");
 
-exports.orderById = (req, res, next, id) => {
-  Order.findById(id)
-    .populate("products.product", "name price")
-    .exec((error, order) => {
-      if (error || !order) return res.status(400).json({ error: errorHandler });
-
-      req.order = order;
-      next();
-    });
+exports.orderById = async (req, res, next, id) => {
+  try {
+    const order = await Order.findById(id).populate("products.product", "name price");
+    if (!order) return res.status(400).json({ error: "Order not found" });
+    req.order = order;
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
-exports.create = (req, res) => {
-  // console.log(req.body);
-  req.body.order.user = req.profile; // req.profile set in userById in controller.user.js
+exports.create = async (req, res) => {
+  req.body.order.user = req.profile;
   const order = new Order(req.body.order);
-  order.save((error, data) => {
-    if (error) return res.status(400).json({ error: errorHandler });
-
+  try {
+    const data = await order.save();
     res.json(data);
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
-exports.addOrderToUserHistory = (req, res, next) => {
-  const history = [];
-  req.body.order.products.forEach((item) => {
-    history.push({
-      _id: item._id,
-      name: item.name,
-      description: item.description,
-      category: item.category,
-      quantity: item.count,
-      transaction_id: req.body.order.transaction_id,
-      amount: req.body.order.amount,
-    });
-  });
-
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $push: { history: history } },
-    { new: true },
-    (error, data) => {
-      if (error)
-        return res
-          .status(400)
-          .json({ error: "Could not update user purchase history" });
-
-      next();
-    }
-  );
+exports.addOrderToUserHistory = async (req, res, next) => {
+  const history = req.body.order.products.map((item) => ({
+    _id: item._id,
+    name: item.name,
+    description: item.description,
+    category: item.category,
+    quantity: item.count,
+    transaction_id: req.body.order.transaction_id,
+    amount: req.body.order.amount,
+  }));
+  try {
+    await User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $push: { history } },
+      { new: true }
+    );
+    next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: "Could not update user purchase history" });
+  }
 };
 
-exports.listOrders = (req, res) => {
-  Order.find()
-    .populate("user", "_id name address")
-    .sort("-created")
-    .exec((error, orders) => {
-      console.log(error);
-      if (error) {
-        return res.status(400).json({
-          error: errorHandler(error),
-        });
-      }
-      res.json(orders);
-    });
+exports.listOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("user", "_id name address")
+      .sort("-createdAt");
+    res.json(orders);
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
 exports.getStatusValue = (req, res) => {
   res.json(Order.schema.path("status").enumValues);
 };
 
-exports.updateStatusOrder = (req, res) => {
-  Order.update(
-    { _id: req.body.orderId },
-    { $set: { status: req.body.status } },
-    (error, order) => {
-      if (error) return res.status(400).json({ error: errorHandler });
-
-      return res.json(order);
-    }
-  );
+exports.updateStatusOrder = async (req, res) => {
+  try {
+    const order = await Order.updateOne(
+      { _id: req.body.orderId },
+      { $set: { status: req.body.status } }
+    );
+    return res.json(order);
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
