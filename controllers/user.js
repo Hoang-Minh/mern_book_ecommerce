bb21a0ad5c04cba24664db073a8534a8cb489632
Minh Nguyen <minh.nguyen@ocit.oc.gov>
2026-05-15 11:08:30 -0700

    fix: async/await in user controller, fix sort field and hashed_password typo

--- controllers/user.js
+++ controllers/user.js
@@ -2,50 +2,47 @@ const User = require("../models/users");
 const { Order } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.userById = (req, res, next, id) => {
-  User.findById(id).exec((error, user) => {
-    if (error || !user) {
-      return res.status(400).json({ error: "User not found" });
-    }
-
+exports.userById = async (req, res, next, id) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) return res.status(400).json({ error: "User not found" });
     req.profile = user;
     next();
-  });
+  } catch (error) {
+    return res.status(400).json({ error: "User not found" });
+  }
 };
 
 exports.read = (req, res) => {
-  req.profile.hased_password = undefined;
+  req.profile.hashed_password = undefined;
   req.profile.salt = undefined;
-
   return res.json(req.profile);
 };
 
-exports.update = (req, res) => {
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (error, user) => {
-      if (error)
-        return res
-          .status(400)
-          .json({ error: "You are not authorized to perform this action" });
-
-      req.profile.hased_password = undefined;
-      req.profile.salt = undefined;
-      res.json(user);
-    }
-  );
+exports.update = async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $set: req.body },
+      { new: true }
+    );
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: "You are not authorized to perform this action" });
+  }
 };
 
-exports.purchaseHistory = (req, res) => {
-  console.log("purchase history", req.profile._id);
-  Order.find({ user: req.profile._id })
-    .populate("user", "_id name")
-    .sort("-created")
-    .exec((error, orders) => {
-      if (error) return res.status(400).json({ error: errorHandler });
-
-      res.json(orders);
-    });
+exports.purchaseHistory = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.profile._id })
+      .populate("user", "_id name")
+      .sort("-createdAt");
+    res.json(orders);
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
