889b246d02bb86ca2ec289d1e354456b920035d6
Minh Nguyen <minh.nguyen@ocit.oc.gov>
2026-05-15 11:08:36 -0700

    fix: async/await, deleteOne, and typo fixes in category controller

--- controllers/category.js
+++ controllers/category.js
@@ -1,60 +1,57 @@
-const Cateogry = require("../models/category");
+const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
-//const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.categoryById = (req, res, next, id) => {
-  Cateogry.findById(id).exec((error, data) => {
-    if (error || !data)
-      return res.status(400).json({ error: "Category does not exist" });
+exports.categoryById = async (req, res, next, id) => {
+  try {
+    const data = await Category.findById(id);
+    if (!data) return res.status(400).json({ error: "Category does not exist" });
     req.category = data;
     next();
-  });
+  } catch (error) {
+    return res.status(400).json({ error: "Category does not exist" });
+  }
 };
 
-exports.create = (req, res, next) => {
-  const category = new Cateogry(req.body);
-
-  category.save((error, data) => {
-    if (error) {
-      res.status(400);
-      return res.status(400).json({ error: errorHandler(error) });
-    }
-
+exports.create = async (req, res) => {
+  const category = new Category(req.body);
+  try {
+    const data = await category.save();
     res.json({ data });
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
 exports.read = (req, res) => {
   return res.json(req.category);
 };
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const category = req.category;
-  console.log(req.body);
   category.name = req.body.name;
-  category.save((error, data) => {
-    if (error || !data)
-      return res.status(400).json({ error: errorHandler(error) });
-
+  try {
+    const data = await category.save();
     res.json(data);
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
 
-exports.remove = (req, res) => {
+exports.remove = async (req, res) => {
   const category = req.category;
-
-  category.remove((error, data) => {
-    if (error || !data)
-      return res.satus(400).json({ error: errorHandler(error) });
-
+  try {
+    await category.deleteOne();
     res.json({ message: "Category deleted" });
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
-exports.list = (req, res) => {
-  Cateogry.find().exec((error, data) => {
-    if (error || !data)
-      return res.status(400).json({ error: errorHandler(error) });
 
+exports.list = async (req, res) => {
+  try {
+    const data = await Category.find();
     res.json(data);
-  });
+  } catch (error) {
+    return res.status(400).json({ error: errorHandler(error) });
+  }
 };
